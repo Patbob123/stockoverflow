2578c94fc8487a7519cb0eedc449581ec750db19
Bohan Zhang <bohann.zhang@mail.utoronto.ca>
2025-11-24 22:34:33 -0500

    User

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,26 +1,32 @@
 package entities;
 
-// import lombok.Getter;
-// import lombok.Setter;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
-// @Getter
-// @Setter
+@Getter
 public class User {
+    // Getters and Setters
     private final String userID;
+    @Setter
     private String username;
+    @Setter
     private String password;
+    @Setter
     private String email;
+    @Setter
     private PortfolioList portfolioList;
+    @Setter
     private List<String> searchHistory;
+    @Setter
+    private boolean isLoggedIn;
 
-    public User(String userID, String username/*, Date date, ImageIcon image, double high, double low, double close*/){
+    public User(String userID, String username, String password) {
         this.userID = userID;
         this.username = username;
+        this.password = this.password;
+        this.isLoggedIn = false;
     }
-
-
-
 
 }
